f1101a07e251b1c4d4e487a99c715cd868712439
maverick <292862808@qq.com>
2025-08-16 00:10:29 +0800

    first

--- boot3-01-demo/src/main/java/com/atguigu/boot/MainApplication.java
+++ boot3-01-demo/src/main/java/com/atguigu/boot/MainApplication.java
@@ -1,4 +1,16 @@
 package com.atguigu.boot;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author maverick
+ * @Description 启动springboot的主入口程序
+ * @create 2025年8月14日20:32:30
+ */
+@SpringBootApplication // 这是一个springboot的启动类
 public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
--- boot3-01-demo/src/main/java/com/atguigu/boot/controller/HelloController.java
+++ boot3-01-demo/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -1,4 +1,18 @@
 package com.atguigu.boot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: hello world
+ * @Author: maverick
+ * @Date: 2025年8月14日20:41:21
+ **/
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
 }
